8e616c8766c44893190fd370f55009f95b2ba62b
Pratyush Kumar Jha <122214040+UniteUniverse@users.noreply.github.com>
2025-05-04 17:28:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,5 @@ def get_recommendations():
     return jsonify({"recommended_assessments": formatted_results}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))  
+    app.run(host='0.0.0.0', port=port)
