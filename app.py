580f2aef32af8374394810476d102b12aa09a206
Pratyush Kumar Jha <122214040+UniteUniverse@users.noreply.github.com>
2025-05-04 18:13:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from recommendation_logic import recommend_assessments
+from waitress import serve
+
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS if needed
@@ -34,5 +36,5 @@ def get_recommendations():
     return jsonify({"recommended_assessments": formatted_results}), 200
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))  
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    serve(app, host='0.0.0.0', port=port)
